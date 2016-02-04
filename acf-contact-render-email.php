34d91e0e93660a453ef4fac37d8334ec42777408
cpham <cpham@adcetera.com>
2016-02-04 13:53:08 -0600

    adding customer email template / adding key validation

--- acf-contact-render-email.php
+++ acf-contact-render-email.php
@@ -42,6 +42,7 @@
 
 		return $message;
 	}
+		
 
 	function acf_cf_format_message($value, $type, $field, $post_id) {
 		$default = '<a href="'.get_edit_post_link($post_id) .'">Click here to view in admin.</a>';
@@ -49,9 +50,7 @@
 		switch($type) {
 			case "repeater":
 
-					$message .= acf_cf_format_subfields($value, $field['key'], $field['sub_fields'], $field['label'], $post_id);
-
-
+				$message .= acf_cf_format_subfields($value, $field['key'], $field['sub_fields'], $field['label'], $post_id);
 
 				break;
 			case "post_object":
@@ -172,6 +171,8 @@
 			default:
 				$message .= $value;
 		}
+		
+		
 
 		return $message;
 
@@ -183,50 +184,135 @@
 				add_action('template_redirect', 'acf_cf_email_template');
 
 	}
+	
+	
+	if(!empty($_GET['acf-cf-customeremail'])) {
+
+				add_action('template_redirect', 'acf_cf_customeremail_template');
+
+	}
+	
+	// ADMIN EMAIL TEMPLATE
 
 	function acf_cf_email_template() {
-		global $post;
+		global $post;	
+
 		$post_id = $_GET['acf-cf-email'];
 
+		$key = get_post_meta($post_id, 'acfcf-key', true);
 		$post = get_post($post_id);
+		if($_GET['key'] == $key) { // CHECK FOR  KEY
 
-		setup_postdata($post);
+			setup_postdata($post);
+	
+			if(empty($_GET['template'])) {
+				echo '<h3 style="text-align: center;">' . get_the_title($post->ID) . '</h3>';
+				$fields = get_field_objects($post_id);
+	
+				
+				$message = '<table class="table table-striped" style="width: 100%; font-family: helvetica,arial;">';
+				foreach($fields as $field) {
+					if(!empty($field['value'])) {
+						$message .= '<tr>';
+						$message .= '<td style="padding: 5px; width: 20%; text-align: right;" align="right" valign="top"><strong>' . $field['label'] . ': </strong></td>';
+	
+						$value = $field['value'];
+	
+	
+	
+						$message .= '<td  style="padding: 5px;" align="left" valign="top">';
+	
+						$message .= acf_cf_format_message($field['value'], $field['type'], $field, $post_id);
+						
+						$message .= '</td>';
+						$message .= '</tr>';
+	
+					}
+				}
+				
+				$message .= '<tr><td  style="padding: 5px;" align="right" valign="top"><strong>IP:</strong></td><td>'. $_SERVER['REMOTE_ADDR'] . '</td></tr>';
+	
+				$message .= '</table>';
+				
+				echo $message;
+	
+	
+	
+			} else {
+				$template = $_GET['template'];
+				$template = get_template_directory() . '/acf-cf-templates/acf-cf-' . $template . '.php';
+				require_once($template);
+	
+			}
+		}
+		die();
 
-		if(empty($_GET['template'])) {
-			echo '<h3 style="text-align: center;">' . get_the_title($post->ID) . '</h3>';
-			$fields = get_field_objects($post_id);
+	}
+	
+	// CUSTOMER EMAIL TEMPLATE
+	
+	function acf_cf_customeremail_template() {
+		global $post;
+		$post_id = $_GET['acf-cf-customeremail'];
+		$key = get_post_meta($post_id, 'acfcf-key', true);
+		$post = get_post($post_id);		
+		
+		if($_GET['key'] == $key) { // CHECK FOR  KEY
+	
+			setup_postdata($post);
+			$forms = get_field('forms','option');
 
-			$message = '<table class="table table-striped" style="width: 100%; font-family: helvetica,arial;">';
-			foreach($fields as $field) {
-				if(!empty($field['value'])) {
-					$message .= '<tr>';
-					$message .= '<td style="padding: 5px; width: 20%; text-align: right;" align="right" valign="top"><strong>' . $field['label'] . ': </strong></td>';
+			$post_type = get_post_field('post_type', $post_id);
 
-					$value = $field['value'];
-
-
-
-					$message .= '<td  style="padding: 5px;" align="left" valign="top">';
-
-					$message .= acf_cf_format_message($field['value'], $field['type'], $field, $post_id);
-					$message .= '</td>';
-					$message .= '</tr>';
-
+			$title = get_the_title($post_id);
+			if(!empty($forms)) {
+				foreach($forms as $form) {
+					if($post_type == $form['post_type']) {
+						$title = $form['customertitle'];
+					}
 				}
 			}
-			$message .= '</table>';
+		
 
-			echo $message;
-
-
-
-		} else {
-			$template = $_GET['template'];
-			$template = get_template_directory() . '/acf-cf-templates/acf-cf-' . $template . '.php';
-			require_once($template);
-
+			if(empty($_GET['customertemplate'])) {
+				echo '<h3 style="text-align: center;">' . $title . '</h3>';
+				$fields = get_field_objects($post_id);
+	
+				$message = '<table class="table table-striped" style="width: 100%; font-family: helvetica,arial;">';
+				foreach($fields as $field) {
+					if(!empty($field['value'])) {
+						$message .= '<tr>';
+						$message .= '<td style="padding: 5px; width: 20%; text-align: right;" align="right" valign="top"><strong>' . $field['label'] . ': </strong></td>';
+	
+						$value = $field['value'];
+	
+	
+	
+						$message .= '<td  style="padding: 5px;" align="left" valign="top">';
+	
+						$message .= acf_cf_format_message($field['value'], $field['type'], $field, $post_id);
+						
+						$message .= '</td>';
+						$message .= '</tr>';
+	
+					}
+				}
+				
+				$message .= '<tr><td  style="padding: 5px;" align="right" valign="top"><strong>IP:</strong></td><td>'. $_SERVER['REMOTE_ADDR'] . '</td></tr>';
+	
+				$message .= '</table>';
+				
+				echo $message;
+	
+	
+	
+			} else {
+				$template = $_GET['customertemplate'];
+				$template = get_template_directory() . '/acf-cf-templates/acf-cf-' . $template . '.php';
+				require_once($template);
+	
+			}
 		}
-
 		die();
 
 	}
